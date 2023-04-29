f0a77d8ff3f75baf020a911801abcd7ae8665052
ColinChan520 <a1864874@adelaide.edu.au>
2023-04-29 20:29:40 +0930

    connmiy

---
+++ BigNumCalc.cpp
@@ -0,0 +1,99 @@
+#include "BigNumCalc.h"
+
+using namespace std;
+
+list<int> BigNumCalc :: buildBigNum(string numString){
+    list<int> bigNum;
+    for (char c: numString) {
+        int a = c - '0';
+        bigNum.push_back(a);
+    }
+    return bigNum;
+}
+
+list<int> BigNumCalc :: add(list<int> num1, list<int> num2){
+    auto currentNum1 = prev(num1.end());
+    auto currentNum2 = prev(num2.end());
+    list<int> result;
+    int carry = 0;
+    while(currentNum1 != num1.begin() && currentNum2 != num2.begin()){
+        int currentResult =  *currentNum1 + *currentNum2 + carry;
+        if(currentResult >= 10){
+            carry = 1;
+            int endNum = currentResult % 10;
+            result.push_back(endNum);
+        }
+        else{
+            carry = 0;
+            result.push_back(currentResult);
+        }
+        currentNum1 = prev(currentNum1);
+        currentNum2 = prev(currentNum2);
+    }
+    int beginNum = *currentNum1 + *currentNum2 + carry;
+    if(beginNum >= 10){
+        result.push_back(beginNum % 10);
+        result.push_back(1);
+    }  
+    else{
+        result.push_back(beginNum);
+    }
+    result.reverse();
+    return result;
+}
+
+list<int> BigNumCalc :: sub(list<int> num1, list<int> num2){
+    auto currentNum1 = prev(num1.end());
+    auto currentNum2 = prev(num2.end());
+    list<int> result;
+    int carry = 0;
+    while(currentNum1 != num1.begin() && currentNum2 != num2.begin()){
+        int currentResult =  *currentNum1 - *currentNum2 - carry;
+        if(currentResult < 0){
+            carry = 1;
+            int endNum = currentResult + 10;
+            result.push_back(endNum);
+        }
+        else{
+            carry = 0;
+            result.push_back(currentResult);
+        }
+        currentNum1 = prev(currentNum1);
+        currentNum2 = prev(currentNum2);
+    }
+    int beginNum = *currentNum1 - *currentNum2 - carry;
+    result.push_back(beginNum);
+    result.reverse();
+    return result;
+}
+
+list<int> BigNumCalc :: mul(list<int> num1, list<int> num2){
+    auto currentNum1 = prev(num1.end());
+    auto currentNum2 = prev(num2.end());
+    list<int> result;
+    int carry = 0;
+    while(currentNum1 != num1.begin()){
+        int currentResult =  *currentNum1 * *currentNum2 + carry;
+        if(currentResult >= 10){
+            carry = currentResult / 10;
+            int endNum = currentResult % 10;
+            result.push_back(endNum);
+        }
+        else{
+            carry = 0;
+            result.push_back(currentResult);
+        }
+        currentNum1 = prev(currentNum1);
+        currentNum2 = prev(currentNum2);
+    }
+    int beginNum = *currentNum1 * *currentNum2 + carry;
+    if(beginNum >= 10){
+        result.push_back(beginNum % 10);
+        result.push_back(beginNum / 10);
+    }  
+    else{
+        result.push_back(beginNum);
+    }
+    result.reverse();
+    return result;
+}
---
+++ BigNumCalc.h
@@ -0,0 +1,16 @@
+#ifndef BIGNUMCALC_H
+#define BIGNUMCALC_H
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+class BigNumCalc{
+    list<int> buildBigNum(string numString);
+    list<int> add(list<int> num1, list<int> num2);
+    list<int> sub(list<int> num1, list<int> num2);
+    list<int> mul(list<int> num1, list<int> num2);
+};
+
+#endif

743ff64218bdf5d5512f72a7031d9e2eeef96e62
ColinChan520 <a1864874@adelaide.edu.au>
2023-04-30 14:02:00 +0930

    111

--- BigNumCalc.h
+++ BigNumCalc.h
@@ -7,6 +7,7 @@
 using namespace std;
 
 class BigNumCalc{
+    public:
     list<int> buildBigNum(string numString);
     list<int> add(list<int> num1, list<int> num2);
     list<int> sub(list<int> num1, list<int> num2);
